a91dbbfa363f95c748bb998da82cb2a1fb4a8840
Esteban Mesa <esteban2002mesa@gmail.com>
2023-06-08 17:56:33 -0500

    preventivo > contando TODO

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,7 +1,24 @@
 import "./createTodoButton.css";
 
 function CreateTodoButton() {
-  return <button className="createTodo">➕</button>;
+  return (
+    <button
+      className="createTodo"
+      onClick={(event) => {
+        click(event);
+      }}
+    >
+      ➕
+    </button>
+  );
+}
+
+function click(thisEvent) {
+  console.log(
+    `evento: ${thisEvent}
+    target: ${thisEvent.target}`,
+  );
+  console.log(thisEvent);
 }
 
 export { CreateTodoButton };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,24 @@
+import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="todoSearch" placeholder="Cortar cebolla"></input>;
+  const [searchValue, setSearchValue] = React.useState("");
+
+  return (
+    <input
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+        change(event);
+      }}
+      value={searchValue}
+      className="todoSearch"
+      placeholder="Cortar cebolla"
+    ></input>
+  );
+}
+
+function change(eve) {
+  console.log(`esta escrito: [${eve.target.value}]`);
 }
 
 export { TodoSearch };
